8753a83dfdefdc04c55f0613a0e65b12e2194145
backendCoderHouse <128253477+backendCoderHouse@users.noreply.github.com>
2023-03-18 15:42:43 -0300

    "Uploadde Files"

---
+++ ejemploScopes.js
@@ -0,0 +1,16 @@
+const number = 1;
+
+function ejemplo(){
+
+let saludo = 'hola'
+
+console.log(saludo)
+
+}
+
+ejemplo()
+
+
+console.log(number)
+console.log(saludo)
+
---
+++ metodosDeConsola.js
@@ -0,0 +1,18 @@
+// CODER HOUSE PROGRAMACIÓN BACKEND CLASE 0
+
+// console.log() método
+
+console.log('css is awesome') // string
+
+console.log(1) // number
+
+console.log(true) // boolean
+
+console.log(null)
+
+console.log(undefined)
+
+console.log([1, 2, 3, 4]) // array
+
+//object
+console.log({ serie: 'the last of us', estreno: '2023-01-15', plataforma: 'HBO', genero: 'apocaliptico' })
